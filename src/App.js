4395f07de8739bd45db57484b1c7ecdf3c6c81e0
anveshreddy <72124154+anveshredddy@users.noreply.github.com>
2022-07-11 17:57:19 +0530

    tech1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//JSX arrow function which returns a h1,p and ul elements
+
+const JSX = () =>{
+  return(
+    <div>
+    <h1> Hello world</h1>
+    <p>Three li elements are</p>
+    <ul>
+      <li>1st li element</li>
+      <li>2nd li element</li>
+      <li>3rd li element</li>
+    </ul>
+    </div>
+  )
+}
+
+// TypesOfFruit arrow function which returns different fruits
+//child of fruit
+
+const TypesOfFruit = () =>{
+  return(
+    <div>
+      <h2>Fruits</h2>
+      <ul>
+        <li>Mango</li>
+        <li>Pine Apple</li>
+        <li>Orange</li>
+        <li>Custard Apple</li>
+        <li>Apple</li>
+        <li>Banana</li>
+      </ul>
+    </div>
+  )
+}
+
+//Fruit arrow function which contain TypesOffruit function
+//child of TypesOfFood
+
+const Fruits =() =>{
+  return(
+    <div>
+      <TypesOfFruit/>
+    </div>
+  )
+}
+
+// TypesOfFood functional component it contains JSX and Fruits arrrow functions
+
+function TypesOfFood() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JSX/>
+      <Fruits/>     
     </div>
   );
 }
 
-export default App;
+export default TypesOfFood;
